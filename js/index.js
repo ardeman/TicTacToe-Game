0a11002e0894cc8476acc52db27a222951300da3
Ardeman <are.theman@gmail.com>
2019-12-12 15:56:31 +0700

    fix indentation

--- js/index.js
+++ js/index.js
@@ -7,31 +7,31 @@ $(document).ready(function ()
     var o_win = $("#o_win").text(); // Store #o_win text to its variable
     var x_win = $("#x_win").text(); // Store #x_win text to its variable
     var turnIndicator = $("#turn-indicator"); // Store #turn-indicator in a variable for easy access
-    
+
     // Capture #boardsize-form value to determine board size and set up the board
-    $( "#boardsize-form" ).submit(function( event )
+    $("#boardsize-form").submit(function (event)
     {
         event.preventDefault(); // Prevent from page reload after form is submitted
         boardSize = parseInt($("#boardsize-input").val()); // Set #boardsize-input according to its value parsed to integer
         numSquares = (boardSize * boardSize); // Set numSquares value, which is #boardsize-input squared
-        
+
         gameStart(); // Start the game
-        
+
         $("#boardsize").hide(); // Hide #boardsize
         $("#play").show(); // Show #play
     });
-    
+
     // Capture #reset button when clicked
     $("#reset").click(function ()
     {
         gameStart(); // Restart the game
     });
-    
+
     function gameStart()
     {
         console.clear();
         board.html(""); // Clear the board
-        
+
         // To make scalable, set wrapper #game width and height 82px * boardSize
         board.css({
             "width": ((82) * boardSize) + 'px',
@@ -41,42 +41,42 @@ $(document).ready(function ()
         // Iterate over numSquares, for every index in numSquares, print to document a "li"
         for (var i = 0; i < numSquares; i++)
         {
-            board.append('<li id="'+String(i)+'" class="btn span1 square" data-ttt="" >+</li>'); // Need to append or else "li" overwrite each other!!
+            board.append('<li id="' + String(i) + '" class="btn span1 square" data-ttt="" >+</li>'); // Need to append or else "li" overwrite each other!!
         }
-        
+
         var squares = $(".square"); // Store all square "li" in a variable
-        
-        turnIndicator.css("color","black").text("O's Turn"); // After board is made, indicate who goes first
-        
+
+        turnIndicator.css("color", "black").text("O's Turn"); // After board is made, indicate who goes first
+
         var boardClicks = 0; // Declare a click counter
-        
+
         // If board is clicked, increment click counter
         board.on("click", "li", function ()
         {
             // determineWinner will return true if it finds a winning combination
             if (determineWinner(true))
             {
-                turnIndicator.css("color","blue").text(winningPlayer[0].toUpperCase() + ' wins!'); // Show who is the winner in the turnIndicator
-                
+                turnIndicator.css("color", "blue").text(winningPlayer[0].toUpperCase() + ' wins!'); // Show who is the winner in the turnIndicator
+
                 // If after won, player continues clicking, then restart the game
-                if(boardClicks === 0)
+                if (boardClicks === 0)
                 {
                     alert(winningPlayer[0].toUpperCase() + ' has won the game. Start a new game');
                     gameStart();
-                    
+
                     return;
                 }
                 else
                 {
                     alert(winningPlayer[0].toUpperCase() + ' wins');
                     boardClicks = 0;
-                
-                    if(winningPlayer[0] === 'o')
+
+                    if (winningPlayer[0] === 'o')
                     {
                         o_win++;
                         $('#o_win').text(o_win);
                     }
-                    else if(winningPlayer[0] === 'x')
+                    else if (winningPlayer[0] === 'x')
                     {
                         x_win++;
                         $('#x_win').text(x_win);
@@ -85,28 +85,28 @@ $(document).ready(function ()
             }
             else if (isEven(boardClicks))
             {
-                turnIndicator.css("color","red").text("X's Turn");
+                turnIndicator.css("color", "red").text("X's Turn");
                 boardClicks++;
             }
             else
             {
-                turnIndicator.css("color","black").text("O's Turn");
+                turnIndicator.css("color", "black").text("O's Turn");
                 boardClicks++;
             }
         });
-        
+
         // Make an array to hold square click data
         var squareClicks = [];
-        
+
         // Set squareclick data for each square to 0
         for (var i = 0; i < numSquares; i++)
         {
             squareClicks[i] = 0;
         }
-        
+
         // Make a variable to store winning combination
         var winningPlayer;
-        
+
         // Add function to determine winner based on clicks array
         function determineWinner(printConsole = true)
         {
@@ -121,8 +121,8 @@ $(document).ready(function ()
                     {
                         rowCheck.push(squares.eq(squareNum).data("ttt"));
                     }
-                    
-                    if(printConsole)
+
+                    if (printConsole)
                     {
                         console.log('Row ' + rowNum++, rowCheck);
                         console.log(allSame(rowCheck));
@@ -135,7 +135,7 @@ $(document).ready(function ()
                     }
                 }
             }
-            
+
             // Check for win by column
             var colNum = 1;
             for (i = 0; i < numSquares; i += 1)
@@ -147,8 +147,8 @@ $(document).ready(function ()
                     {
                         colCheck.push(squares.eq(squareNum).data("ttt"));
                     }
-                    
-                    if(printConsole)
+
+                    if (printConsole)
                     {
                         console.log('Column ' + colNum++, colCheck);
                         console.log(allSame(colCheck));
@@ -161,7 +161,7 @@ $(document).ready(function ()
                     }
                 }
             }
-            
+
             // Check for win by left diagonal
             var diag1Check = []; // Needs to be outside of for loop to prevent overwriting array
             for (i = 0; i < numSquares; i += 1)
@@ -173,19 +173,19 @@ $(document).ready(function ()
                     diag1Check.push(squares.eq(i).data("ttt"));
                 }
             }
-            
-            if(printConsole)
+
+            if (printConsole)
             {
                 console.log('Left diagonal', diag1Check);
                 console.log(allSame(diag1Check));
             }
-            
+
             if (allSame(diag1Check))
             {
                 winningPlayer = diag1Check;
                 return true;
             }
-            
+
             // Check for win by right diagonal
             var diag2Check = [];
             for (i = (boardSize - 1); i < (numSquares - 1); i += 1)
@@ -196,20 +196,20 @@ $(document).ready(function ()
                     diag2Check.push(squares.eq(i).data("ttt"));
                 }
             }
-            
-            if(printConsole)
+
+            if (printConsole)
             {
                 console.log('Right diagonal', diag2Check);
                 console.log(allSame(diag2Check));
             }
-            
+
             if (allSame(diag2Check))
             {
                 winningPlayer = diag2Check;
                 return true;
             }
-        }; // End determineWinner function
-        
+        } // End determineWinner function
+
         // Add function to count square clicks
         function countClicks()
         {
@@ -221,16 +221,16 @@ $(document).ready(function ()
             }
             else if (isOdd(boardClicks) && squareClicks[divID] === 1)
             {
-                $(this).addClass('disable x btn-info').data("ttt", "x").text( "x");
+                $(this).addClass('disable x btn-info').data("ttt", "x").text("x");
             }
             else if (!determineWinner(false))
             {
                 alert('Already selected');
                 boardClicks -= 1;
             }
-            
+
             console.log('Turn', squareClicks.reduce(sum));
-            
+
             if (determineWinner(false))
             {
                 for (var i = 0; i < numSquares; i++)
@@ -238,14 +238,14 @@ $(document).ready(function ()
                     squareClicks[i] = 2;
                 }
             }
-        };
-        
+        }
+
         for (var i = 0; i < numSquares; i++)
         {
             squares.eq(i).on("click", countClicks);
         }
     }
-    
+
     function isEven(value)
     {
         if (value % 2 === 0)
@@ -286,8 +286,9 @@ $(document).ready(function ()
             });
         }
     }
-    
-    function sum(total, num) {
+
+    function sum(total, num)
+    {
         return total + num;
     }
 });
